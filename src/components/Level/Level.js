b424c8f93e8c387e29abbb6615c8911aab3c8423
dfsdanimaulana <danimaulana9f@gmail.com>
2021-11-18 18:07:15 +0700

    add new pic

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -5,14 +5,17 @@ export default function Level() {
         <div className='level-grid'>
             <div className='level-card'>
                 <img src='/img/cover.jpg' alt='level' />
+                <img src="/img/locked.png" alt="locked" className="locked" />
                 <h5>Level 1</h5>
             </div>
             <div className='level-card'>
                 <img src='/img/cover.jpg' alt='level' />
+                <img src="/img/locked.png" alt="locked" className="locked" />
                 <h5>Level 2</h5>
             </div>
             <div className='level-card'>
                 <img src='/img/cover.jpg' alt='level' />
+                <img src="/img/locked.png" alt="locked" className="locked" />
                 <h5>Level 3</h5>
             </div>
         </div>
