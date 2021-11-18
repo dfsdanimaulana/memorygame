3dc8c23532780ae1736467334e619e5bfcfdc2b4
dfsdanimaulana <danimaulana9f@gmail.com>
2021-11-18 17:28:22 +0700

    set responsive

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -3,9 +3,18 @@ import './Level.css'
 export default function Level() {
     return (
         <div className='level-grid'>
-            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
-            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
-            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
+            <div className='level-card'>
+                <img src='/img/cover.jpg' alt='level' />
+                <h5>Level 1</h5>
+            </div>
+            <div className='level-card'>
+                <img src='/img/cover.jpg' alt='level' />
+                <h5>Level 2</h5>
+            </div>
+            <div className='level-card'>
+                <img src='/img/cover.jpg' alt='level' />
+                <h5>Level 3</h5>
+            </div>
         </div>
     )
 }
