9e109518a9b74f06cf0b59e65b53668bcd40c936
dfsdanimaulana <danimaulana9f@gmail.com>
2021-11-18 16:51:44 +0700

    recreate timer using useRef

---
+++ src/components/Level/Level.js
@@ -0,0 +1,11 @@
+import './Level.css'
+
+export default function Level() {
+    return (
+        <div className='level-grid'>
+            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
+            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
+            <div className="level-card"><img src="/img/cover.jpg" alt="level" /></div>
+        </div>
+    )
+}
