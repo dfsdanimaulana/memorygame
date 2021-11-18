72e8cebfe3c849766d03439b118ce2b68c6eec61
dfsdanimaulana <danimaulana9f@gmail.com>
2021-11-18 15:32:08 +0700

    add time counter

--- src/App.js
+++ src/App.js
@@ -21,25 +21,50 @@ function App() {
     const [choiceOne, setChoiceOne] = useState(null)
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabled] = useState(false)
+    const [time, setTime] = useState(0)
+    const [count, setCount] = useState(0)
 
     // shuffle cards
     const shuffleCards = () => {
         const shuffeledCards = [...cardImages, ...cardImages]
             .sort(() => Math.random() - 0.5)
             .map((card) => ({ ...card, id: Math.random() }))
+
+        setTime(0)
+        setCount(0)
+        stopCount()
         setChoiceOne(null)
         setChoiceTwo(null)
-        setCards(shuffeledCards)
         setTurns(0)
+        setCards(shuffeledCards)
+    }
+
+    // time counter start
+    const countTimes = () => {
+        setTime((prevTime) => prevTime + 1)
     }
 
     // handle user choice
     const handleChoice = (card) => {
+        if (time === 0) {
+            setCount(setInterval(countTimes, 1000))
+        }
         choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    }
+
+    // stop time counter
+    const stopCount = () => {
+        // clear interval here
+        clearInterval(count)
+        setCount(0)
     }
 
     // compare two cards
     useEffect(() => {
+        //  cek if all matched
+        if (cards.filter((card) => card.matched === false).length === 0) {
+            stopCount()
+        }
         if (choiceOne && choiceTwo) {
             setDisabled(true)
             if (choiceOne.src === choiceTwo.src) {
@@ -74,6 +99,10 @@ function App() {
         <div className='App'>
             <h1>Twice Memory Game</h1>
             <button onClick={shuffleCards}>new game</button>
+            <div className='progress'>
+                <h5>Turns: {turns}</h5>
+                <h5>Time: {time}s</h5>
+            </div>
             <div className='card-grid'>
                 {cards.map((card) => (
                     <SingleCards
@@ -89,7 +118,6 @@ function App() {
                     />
                 ))}
             </div>
-            <h4>Turns : {turns}</h4>
         </div>
     )
 }
